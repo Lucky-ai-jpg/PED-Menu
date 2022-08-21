fe37f54e203fec86133dafc1864801f6f32b5021
Lucky-ai-jpg <111688760+Lucky-ai-jpg@users.noreply.github.com>
2022-08-21 13:38:16 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,22 +1,90 @@
 xui.Class('App', 'xui.Module',{
     Instance:{
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Tools
+            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
-            append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout5")
-            .setItems([{"id":"before", "pos":"before", "size":100, "min":50, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}, {"id":"main", "min":10, "size":450}, {"id":"after", "pos":"after", "size":50, "min":20, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}])
+            
+            append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout5")
+                .setItems([
+                    {
+                        "id" : "before",
+                        "pos" : "before",
+                        "size" : 100,
+                        "min" : 50,
+                        "max" : 400,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    },
+                    {
+                        "id" : "main",
+                        "min" : 10,
+                        "size" : 450
+                    },
+                    {
+                        "id" : "after",
+                        "pos" : "after",
+                        "size" : 50,
+                        "min" : 20,
+                        "max" : 400,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    }
+                ])
             );
-
-            host.ctl_layout5.append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout13")
-            .setItems([{"id":"before", "pos":"before", "size":150, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":true}, {"id":"main", "min":10, "size":530}, {"id":"after", "pos":"after", "size":120, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":false}])
-            .setType("horizontal")
-            , "main");
-
+            
+            host.ctl_layout5.append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout13")
+                .setItems([
+                    {
+                        "id" : "before",
+                        "pos" : "before",
+                        "size" : 150,
+                        "min" : 10,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : true
+                    },
+                    {
+                        "id" : "main",
+                        "min" : 10,
+                        "size" : 530
+                    },
+                    {
+                        "id" : "after",
+                        "pos" : "after",
+                        "size" : 120,
+                        "min" : 10,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    }
+                ])
+                .setType("horizontal"),
+                "main"
+            );
+            
+            host.ctl_layout13.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button8")
+                .setLeft("16.761904761904763em")
+                .setTop("4.571428571428571em")
+                .setWidth("6.019047619047619em")
+                .setHeight("2.8190476190476192em")
+                .setCaption("Button"),
+                "main"
+            );
+            
             return children;
-            // ]]Code created by CrossUI RAD Tools
+            // ]]Code created by CrossUI RAD Studio
         }
     }
 });
